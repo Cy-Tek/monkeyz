7f0fb07c8523d66601fc517faf2665c1d73333a6
Josh Hannaford <hannaford.joshua55@gmail.com>
2023-10-11 16:46:33 -0400

    Began working on the parser

--- src/ast.zig
+++ src/ast.zig
@@ -1,8 +1,25 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+
 const Token = @import("token.zig").Token;
 
 pub const Program = struct {
-    statements: []Statement,
+    const Self = @This();
+
+    allocator: Allocator,
+    statements: ArrayList(Statement),
+
+    pub fn init(alloc: Allocator) Self {
+        return Self{
+            .allocator = alloc,
+            .statements = ArrayList(Statement).init(alloc),
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.statements.deinit();
+    }
 
     pub fn tokenLiteral(self: @This()) []const u8 {
         return if (self.statements.len > 0)
